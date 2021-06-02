f57e211bdd9f5dc152869dd0ea340091bf54bf7e
Admin <hiv-777@mail.ru>
2021-06-02 10:59:59 +0400

    update parsing

--- parsing.py
+++ parsing.py
@@ -12,7 +12,4 @@ def parse(ref, i):
     element_wr = driver.find_element_by_xpath(
         '/html/body/div[1]/div[7]/div[3]/div[4]/div[1]/div[1]/section/article/table/tbody/tr[' + str(count) + ']/td[4]')
     element_wr_value = element_wr.get_attribute('data-value')
-    return str(
-        element_name_value) + u'\u043f\u0440\u043e\u0438\u0433\u0440\u044b\u0432\u0430\u0435\u0442 ' \
-                              u'\u0432\u0441\u0435\u0433\u043e \u0432 ' + str(
-        round(float(element_wr_value))) + u' \u0438\u0433\u0440\u0430\u0445 \u0438\u0437 100'
+    return str(element_name_value) + ' проигрывает всего в ' + str(round(float(element_wr_value))) + ' играх из 100'
